f47bc75e7c5f0ee46fecf3d48769ca18be888c60
Daryl Susilo <slmd06@gmail.com>
2023-08-10 20:14:20 -0700

    Created a recipe to install Chromium instead of Google Chrome because Chromium is available for ARM64.

--- recipes/package_install.rb
+++ recipes/package_install.rb
@@ -3,9 +3,7 @@ dpkg_package 'code_1.81.0-1690979901_arm64.deb' do
   action :install
 end
 
-# dmg_package 'Google Chrome' do
-#   dmg_name 'googlechrome'
-#   file '/home/dsusilo/Dwnld/googlechrome.dmg'
-#   action :install
-# end
+apt_package 'chromium-browser' do
+  action :install
+end
 
