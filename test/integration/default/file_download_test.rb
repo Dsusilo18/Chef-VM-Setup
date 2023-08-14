a7d6b86852d415b7ac3702a78a3d43d2211714be
Daryl Susilo <slmd06@gmail.com>
2023-08-14 10:48:44 -0700

    created a loop for file download.

--- test/integration/default/file_download_test.rb
+++ test/integration/default/file_download_test.rb
@@ -1,13 +1,22 @@
+docs = %w(/home/dsusilo/Downloads/code_1.81.0-1690979901_arm64.deb 
+/home/dsusilo/Pictures/Background/pexels-cats-coming-1543793.jpg 
+/home/dsusilo/Pictures/Background/pexels-photo-16013451.jpeg)
+
 control 'file_download' do
-  describe file('/home/dsusilo/Downloads/code_1.81.0-1690979901_arm64.deb') do
-    it { should exist }
+  docs.each do |doc|
+    describe file(doc) do
+      it { should exist }
+    end
   end
+  # describe file('/home/dsusilo/Downloads/code_1.81.0-1690979901_arm64.deb') do
+  #   it { should exist }
+  # end
 
-  describe file('/home/dsusilo/Pictures/Background/pexels-cats-coming-1543793.jpg') do
-    it { should exist }
-  end
+  # describe file('/home/dsusilo/Pictures/Background/pexels-cats-coming-1543793.jpg') do
+  #   it { should exist }
+  # end
 
-  describe file('/home/dsusilo/Pictures/Background/pexels-photo-16013451.jpeg') do
-    it { should exist }
-  end
+  # describe file('/home/dsusilo/Pictures/Background/pexels-photo-16013451.jpeg') do
+  #   it { should exist }
+  # end
 end
