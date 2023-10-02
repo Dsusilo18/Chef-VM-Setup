9c85e1c6a65c80d88b990921077b5d93f3e1bb69
Daryl Susilo <v-dsusilo@microsoft.com>
2023-10-02 10:29:01 -0700

    Fixed merging changes.

--- recipes/package_install.rb
+++ recipes/package_install.rb
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-dpkg_package "code_#{node['vm-setup']['code_version']}_arm64.deb" do
-  source "/home/dsusilo/Downloads/code_#{node['vm-setup']['code_version']}_arm64.deb"
-=======
 dpkg_package 'code_1.81.0-1690979901_arm64.deb' do
   source "/home/#{node['vm-setup']['user_name']}/Downloads/code_1.81.0-1690979901_arm64.deb"
->>>>>>> 6adb530dc6de1d85eb4ff538178109cd11586040
   action :install
 end
 
