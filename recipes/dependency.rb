468cce52ca1508ae57b7159b99e9cab3a0c30e79
Daryl Susilo <v-dsusilo@microsoft.com>
2023-12-06 17:10:21 -0800

    Created a more in-depth spec-test for dependency recipe. Added more checks in the dependency recipe.

--- recipes/dependency.rb
+++ recipes/dependency.rb
@@ -1,21 +1,34 @@
-ruby_block 'check the attribute values' do
-  block do
-    if node['vm-setup']['bookmark_url_list'] == node['vm-setup']['bookmark_name_list']
-      raise 'ERROR: bookmark url list in attributes must not be empty.'
-    elsif node['vm-setup']['user_name'] == node['vm-setup']['user_pass']
-      raise 'ERROR: user name in attributes must not be empty.'
-    elsif node['vm-setup']['files_list'] == node['vm-setup']['source_list']
-      raise 'ERROR: files list in attributes must not be empty.'
-    elsif node['vm-setup']['upgrade_ubuntu'].nil?
-      raise 'ERROR: flag to upgrade ubuntu must be a boolean.'
-    elsif node['vm-setup']['bookmark_url_list'].empty? || node['vm-setup']['bookmark_name_list'].empty?
-      raise 'ERROR: bookmark url list and bookmark name list in attributes must not be empty arrays.'
-    elsif node['vm-setup']['code_version'].empty? || node['vm-setup']['background'].empty?
-      raise 'ERROR: code version and background must not be empty Strings.'
-    elsif node['vm-setup']['user_name'].empty? || node['vm-setup']['user_pass'].empty?
-      raise 'ERROR: user name and pass in attributes must not be empty Strings.'
-    elsif node['vm-setup']['files_list'].empty? || node['vm-setup']['source_list'].empty?
-      raise 'ERROR: files list and source list in attributes must not be empty arrays.'
-    end
-  end
+
+if (!node['vm-setup']['bookmark_url_list'].is_a?(String) && !node['vm-setup']['bookmark_url_list'].is_a?(Array)) || node['vm-setup']['bookmark_url_list'].empty?
+  raise 'ERROR: bookmark url list in attributes must be a String or Array and not be empty or nil.'
+elsif (!node['vm-setup']['bookmark_name_list'].is_a?(String) && !node['vm-setup']['bookmark_name_list'].is_a?(Array)) || node['vm-setup']['bookmark_name_list'].empty?
+  raise 'ERROR: bookmark name list in attributes must be a String or Array and not be empty or nil.'
+elsif !node['vm-setup']['code_version'].is_a?(String) || node['vm-setup']['code_version'].empty?
+  raise 'ERROR: code version in attributes must be a String and not be empty or nil.'
+elsif !node['vm-setup']['background'].is_a?(String) || node['vm-setup']['background'].empty?
+  raise 'ERROR: background in attributes must be a String and not be empty or nil.'
+elsif !node['vm-setup']['user_name'].is_a?(String) || node['vm-setup']['user_name'].empty?
+  raise 'ERROR: user name in attributes must be a String and not be empty or nil.'
+elsif !node['vm-setup']['user_pass'].is_a?(String) || node['vm-setup']['user_pass'].empty?
+  raise 'ERROR: user pass in attributes must be a String and not be empty or nil.'
+elsif (!node['vm-setup']['files_list'].is_a?(String) && !node['vm-setup']['files_list'].is_a?(Array)) || node['vm-setup']['files_list'].empty?
+  raise 'ERROR: files list in attributes must be a String or Array and not be empty or nil.'
+elsif (!node['vm-setup']['source_list'].is_a?(String) && !node['vm-setup']['source_list'].is_a?(Array)) || node['vm-setup']['source_list'].empty?
+  raise 'ERROR: source list in attributes must be a String or Array and not be empty or nil.'
+elsif node['vm-setup']['upgrade_ubuntu'].nil? || (!node['vm-setup']['upgrade_ubuntu'].kind_of?(TrueClass) && !node['vm-setup']['upgrade_ubuntu'].kind_of?(FalseClass))
+  raise 'ERROR: flag to upgrade ubuntu must be a boolean.'
+elsif node['vm-setup']['bookmark_url_list'] == node['vm-setup']['code_version']
+  raise 'ERROR: bookmark url list in attributes must not be empty.'
+elsif node['vm-setup']['bookmark_name_list'] == node['vm-setup']['bookmark_url_list']
+  raise 'ERROR: bookmark name list in attributes must not be empty.'
+elsif node['vm-setup']['user_name'] == node['vm-setup']['user_pass']
+  raise 'ERROR: user name in attributes must not be empty.'
+elsif node['vm-setup']['user_pass'] == node['vm-setup']['bookmark_name_list']
+  raise 'ERROR: user pass in attributes must not be empty.'
+elsif node['vm-setup']['files_list'] == node['vm-setup']['source_list']
+  raise 'ERROR: files list in attributes must not be empty.'
+elsif node['vm-setup']['code_version'] == node['vm-setup']['files_list']
+  raise 'ERROR: code version in attributes must not be empty.'
+elsif node['vm-setup']['source_list'] == node['vm-setup']['background']
+  raise 'ERROR: source list in attributes must not be empty.'
 end
--- spec/recipes/dependency_spec.rb
+++ spec/recipes/dependency_spec.rb
@@ -3,7 +3,215 @@ require 'spec_helper'
 describe 'vm-setup::dependency' do
   platform 'mac_os_x'
 
-  it 'checks the attributes for validity' do
-    expect(subject).to run_ruby_block('check the attribute values')
+  shared_examples 'should not raise an error' do
+    it 'no error was raised' do
+      expect { subject }.to_not raise_error
+    end
+  end
+
+  describe 'check bookmark list' do
+    context 'bookmark_name_list is not equal to bookmark_url_list' do
+      default_attributes['vm-setup']['bookmark_name_list'] = 'test-name'
+      default_attributes['vm-setup']['bookmark_url_list'] = 'test-url'
+      include_examples 'should not raise an error'
+    end
+    context 'bookmark_name_list is equal to bookmark_url_list' do
+      default_attributes['vm-setup']['bookmark_name_list'] = 'test'
+      default_attributes['vm-setup']['bookmark_url_list'] = 'test'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: bookmark name list in attributes must not be empty.')
+      end
+    end
+    context 'bookmark_url_list is equal to code_version' do
+      default_attributes['vm-setup']['code_version'] = 'test'
+      default_attributes['vm-setup']['bookmark_url_list'] = 'test'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: bookmark url list in attributes must not be empty.')
+      end
+    end
+    context 'bookmark_name_list is empty' do
+      default_attributes['vm-setup']['bookmark_name_list'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: bookmark name list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+    context 'bookmark_url_list is empty' do
+      default_attributes['vm-setup']['bookmark_url_list'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: bookmark url list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+    context 'bookmark_name_list is nil and not a string or array' do
+      default_attributes['vm-setup']['bookmark_name_list'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: bookmark name list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+    context 'bookmark_url_list is nil and not a string or array' do
+      default_attributes['vm-setup']['bookmark_url_list'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: bookmark url list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+  end
+
+  describe 'check files and source list' do
+    context 'files_list is not equal to source_list' do
+      default_attributes['vm-setup']['files_list'] = 'test-file'
+      default_attributes['vm-setup']['source_list'] = 'test-source'
+      include_examples 'should not raise an error'
+    end
+    context 'files_list is equal to source_url_list' do
+      default_attributes['vm-setup']['files_list'] = 'test'
+      default_attributes['vm-setup']['source_list'] = 'test'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: files list in attributes must not be empty.')
+      end
+    end
+    context 'files_list is empty' do
+      default_attributes['vm-setup']['files_list'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: files list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+    context 'source_list is empty' do
+      default_attributes['vm-setup']['source_list'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: source list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+    context 'files_list is nil and not a string or array' do
+      default_attributes['vm-setup']['files_list'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: files list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+    context 'source_list is nil and not a string or array' do
+      default_attributes['vm-setup']['source_list'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: source list in attributes must be a String or Array and not be empty or nil.')
+      end
+    end
+    context 'source_list is equal to background' do
+      default_attributes['vm-setup']['source_list'] = 'test'
+      default_attributes['vm-setup']['background'] = 'test'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: source list in attributes must not be empty.')
+      end
+    end
+  end
+
+  describe 'check user name and pass' do
+    context 'user_name is not equal to user_pass' do
+      default_attributes['vm-setup']['user_name'] = 'test-name'
+      default_attributes['vm-setup']['user_pass'] = 'test-pass'
+      include_examples 'should not raise an error'
+    end
+    context 'user_name is equal to user_pass' do
+      default_attributes['vm-setup']['user_name'] = 'test'
+      default_attributes['vm-setup']['user_pass'] = 'test'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: user name in attributes must not be empty.')
+      end
+    end
+    context 'user_name is empty' do
+      default_attributes['vm-setup']['user_name'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: user name in attributes must be a String and not be empty or nil.')
+      end
+    end
+    context 'user_pass is empty' do
+      default_attributes['vm-setup']['user_pass'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: user pass in attributes must be a String and not be empty or nil.')
+      end
+    end
+    context 'user_name is nil and not a string' do
+      default_attributes['vm-setup']['user_name'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: user name in attributes must be a String and not be empty or nil.')
+      end
+    end
+    context 'user_pass is nil and not a string' do
+      default_attributes['vm-setup']['user_pass'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: user pass in attributes must be a String and not be empty or nil.')
+      end
+    end
+    context 'user_pass is equal to bookmark_name_list' do
+      default_attributes['vm-setup']['user_pass'] = 'test'
+      default_attributes['vm-setup']['bookmark_name_list'] = 'test'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: user pass in attributes must not be empty.')
+      end
+    end
+  end
+
+  describe 'check background' do
+    context 'background is not equal to upgrade_ubuntu' do
+      default_attributes['vm-setup']['background'] = 'test-url.com'
+      default_attributes['vm-setup']['upgrade_ubuntu'] = false
+      include_examples 'should not raise an error'
+    end
+    context 'background is empty' do
+      default_attributes['vm-setup']['background'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: background in attributes must be a String and not be empty or nil.')
+      end
+    end
+    context 'background is nil and not a string' do
+      default_attributes['vm-setup']['background'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: background in attributes must be a String and not be empty or nil.')
+      end
+    end
+  end
+
+  describe 'check code_version' do
+    context 'code_version is not equal to files_list' do
+      default_attributes['vm-setup']['code_version'] = 'version123'
+      default_attributes['vm-setup']['files_list'] = 'test-files'
+      include_examples 'should not raise an error'
+    end
+    context 'code_version is equal to files_list' do
+      default_attributes['vm-setup']['code_version'] = 'test-files'
+      default_attributes['vm-setup']['files_list'] = 'test-files'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: code version in attributes must not be empty.')
+      end
+    end
+    context 'code_version is empty' do
+      default_attributes['vm-setup']['code_version'] = ''
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: code version in attributes must be a String and not be empty or nil.')
+      end
+    end
+    context 'code_version is nil and not a string' do
+      default_attributes['vm-setup']['code_version'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: code version in attributes must be a String and not be empty or nil.')
+      end
+    end
+  end
+
+  describe 'check upgrade ubuntu' do
+    context 'upgrade ubuntu is nil' do
+      default_attributes['vm-setup']['upgrade_ubuntu'] = nil
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: flag to upgrade ubuntu must be a boolean.')
+      end
+    end
+    context 'upgrade ubuntu is not boolean' do
+      default_attributes['vm-setup']['upgrade_ubuntu'] = 'test'
+      it 'Should raise an error' do
+        expect { subject }.to raise_error('ERROR: flag to upgrade ubuntu must be a boolean.')
+      end
+    end
+    context 'upgrade ubuntu is not nil' do
+      default_attributes['vm-setup']['upgrade_ubuntu'] = true
+      it 'Should not raise an error' do
+        expect { subject }.to_not raise_error
+      end
+    end
   end
 end
